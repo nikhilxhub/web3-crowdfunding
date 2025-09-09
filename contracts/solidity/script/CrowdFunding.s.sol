d416f08859e47cd46804ac4624b349cfc2b9e17f
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-09-09 23:27:16 +0530

    forge

---
+++ contracts/solidity/script/CrowdFunding.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {CrowdFunding} from "../src/CrowdFunding.sol";
+
+contract CrowdFundingScript is Script {
+    
+}
