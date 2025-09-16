6f4104ab82f1e01998d8142336cff02c1c494e8f
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-09-16 13:22:37 +0530

    profile icon

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Crown } from "lucide-react";
+import { Crown, User } from "lucide-react";
 import ThemeToggleButton from "./ui/theme-toggle-button";
 import { ConnectButton } from "thirdweb/react";
 import { Button } from "./ui/button";
@@ -7,19 +7,22 @@ import { toast } from "sonner";
 import { client } from "../client";
 import { useStateContext } from "@/context/StateProvider";
 import { useNavigate } from "react-router-dom";
-
+import {
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+} from "@/components/ui/avatar";
 
 export const NavBar = () => {
   const { address } = useStateContext();
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleCreate = () => {
     if (!address) {
       toast("Please connect your wallet to create a campaign ⚡");
       return;
     }
-    // onCreateClick();
-    navigate("/createCampaign")
+    navigate("/createCampaign");
   };
 
   return (
@@ -28,20 +31,28 @@ export const NavBar = () => {
         <Crown className="size-10" /> CrowdFund
       </div>
 
-      <div className="flex gap-2">
-       
+      <div className="flex gap-4 items-center">
+        <ConnectButton client={client} />
 
-      <ConnectButton client={client} />
+        <Button onClick={handleCreate}>Create Campaign</Button>
 
-      <Button onClick={handleCreate}>Create Campaign</Button>
+        <div
+          onClick={() => navigate("/profile")}
+          className="cursor-pointer hover:opacity-80 transition-opacity"
+        >
+          {/* <Avatar>
+            <AvatarImage
+              src={`https://api.dicebear.com/7.x/identicon/svg?seed=${address}`}
+              alt="User Avatar"
+            />
+            <AvatarFallback>
+            </AvatarFallback>
+            </Avatar> */}
+            <User />
+        </div>
 
-      <Button onClick={() => navigate("/profile")}>Profile</Button>
-
-      <ThemeToggleButton />
-
+        <ThemeToggleButton />
       </div>
-
-
     </nav>
   );
 };
